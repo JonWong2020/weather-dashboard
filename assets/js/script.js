b2777ab38d6e157016e46576ebc912eba588c97c
JonWong2020 <jonathannwong50@aol.com>
2022-09-06 21:31:30 -0700

    added function to fetch data for current city

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,18 @@ var searchHistoryList = [];
 
 // function to get current city weather
 function currentCityWeather(city) {
-    var queryURL = ''
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET',
+      }).then(function (cityWeatherResponse) {
+        console.log(cityWeatherResponse);
+        
+        $("#weatherContent").css("display", "block");
+        $("#cityDetail").empty();
+
+      });
 }
 
 // event listener on search click
@@ -16,7 +27,7 @@ $('#searchBtn').on("click", function(event){
     currentCityWeather(city);
     if (!searchHistoryList.includes(city)) {
         searchHistoryList.push(city);
-        var searchedCity = $('<li class="list-group-item>${city}</li>');
+        var searchedCity = $(`<li class="list-group-item>${city}</li>`);
         $('#searchHistory').append(searchedCity);
     };
 
